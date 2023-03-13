41eb7819ed4949af5f1f8b6c03fc4991f44043b8
Collins Kimutai <collinskimutai72@gmail.com>
2023-03-13 08:46:45 +0300

    getting values from form

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-dayOfWeek = () => {
-    
-}
+const form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+  // Your code here
+  event.preventDefault(); // Prevent the form from submitting and reloading the page
+  const birthdayInput = document.querySelector('#exampleInputEmail1');
+  const genderInput = document.querySelector('#exampleFormControlSelect1');
+
+  const birthday = birthdayInput.value;
+  const gender = genderInput.value;
+
+  
+
+});
